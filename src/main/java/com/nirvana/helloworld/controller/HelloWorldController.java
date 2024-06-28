e3c7a93ce3adf821f81fd77f22add053def1ffbb
Nirvana Dogra <nirvanadogra004@gmail.com>
2024-06-27 23:10:48 -0500

    intial api

---
+++ src/main/java/com/nirvana/helloworld/controller/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.nirvana.helloworld.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping(value={"", "/"})
+    public ResponseEntity<String> displayHomePage() {
+        return ResponseEntity.ok("Hello World!");
+    }
+}
